f73a6db7e14687cfb83e26c19f58639be6bf844f
lsoto <lsoto27@gmail.com>
2016-11-09 09:40:02 -0600

    Added locale strings for es and fr

--- app/src/main/java/com/example/lsoto/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/lsoto/myfirstapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lsoto.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -14,11 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get a string resource from your app's Resources
+        String hello = getResources().getString(R.string.hello_world);
+        // Write a Log
+        Log.d(MainActivity.class.getSimpleName(), hello);
     }
 
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-        // create a new INtent
+        // create a new Intent
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         // get a reference of the edit text view
         EditText editText = (EditText) findViewById(R.id.edit_message);
